ee3a95a51ba93b51b9eb2ff3c069ab75faa32b92
O-mobola <ssalawudeen06@gmail.com>
2020-03-25 18:01:48 +0100

    initial commit of the task to master

---
+++ area_of_a_circle.py
@@ -0,0 +1,7 @@
+#This program calculate the area of a circle
+#Formula is A = πr²
+
+pi = 3.14
+radius = float(input("Enter your radius: "))
+area = pi * radius**2
+print('The area of the circle with radius ',radius,' is', area)
